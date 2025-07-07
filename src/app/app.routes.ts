38d7245f9d5cac26b2afef57bf1ce7b288c8f682
HugCarti <Hleplingard04@gmail.com>
2025-07-08 00:06:04 +0200

    add pages and component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,25 @@
 import { Routes } from '@angular/router';
+import { Home } from './pages/home/home';
+import { Contact } from './pages/contact/contact';
+import { Projects } from './pages/projects/projects';
+import { Studies } from './pages/studies/studies';
+import { Error404 } from './pages/error404/error404';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { 
+        path: '',
+        component: Home
+    },{ 
+        path: 'contact',
+        component: Contact
+    },{ 
+        path: 'projects',
+        component: Projects
+    },{ 
+        path: 'studies',
+        component: Studies
+    },{
+        path: '**',
+        component: Error404
+    }
+];
---
+++ src/app/pages/studies/studies.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-studies',
+  imports: [],
+  templateUrl: './studies.html',
+  styleUrl: './studies.scss'
+})
+export class Studies {
+
+}
